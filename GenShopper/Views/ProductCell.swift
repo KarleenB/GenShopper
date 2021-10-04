c140ad909e5fdfbcff302b5a575bc236cca514ab
Branden Karleen <karleen@MacBook-Karleen.local>
2021-10-04 16:33:35 -0700

    create collectionView and cell

---
+++ GenShopper/Views/ProductCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProductCell.swift
+//  GenShopper
+//
+//  Created by Branden Karleen on 10/4/21.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
