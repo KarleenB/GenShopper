a04fb819c676fc1f3fdaf33d4be7e2323189872c
Branden Karleen <karleen@MacBook-Karleen.local>
2021-10-04 20:48:32 -0700

    add product model

---
+++ GenShopper/Models/Product.swift
@@ -0,0 +1,20 @@
+//
+//  Product.swift
+//  GenShopper
+//
+//  Created by Branden Karleen on 10/4/21.
+//
+
+import Foundation
+
+struct Product {
+    private(set) public var title: String
+    private(set) public var price: String
+    private(set) public var imageName: String
+    
+    init(title: String, price: String, imageName: String) {
+        self.title = title
+        self.price = price
+        self.imageName = imageName
+    }
+}
